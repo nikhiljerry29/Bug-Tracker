e840a62e63ac5938af69686b33063230c106020a
NIKHIL GUPTA <nikhil.gupta5667@gmail.com>
2021-01-30 13:11:13 +0530

    Add Sequelize

--- src/app.js
+++ src/app.js
@@ -1,37 +1,60 @@
-const express = require("express");
-const app = express();
-app.use(express.static("public"));
+require('dotenv').config()
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const { Sequelize, DataTypes, QueryTypes } = require('sequelize')
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
 
-const date = require(__dirname + "/date.js");
-const sqlConnection = require(__dirname + "/sqlConnection.js");
+const date = require(__dirname + "/date.js")
+
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
+	host: 'localhost',
+	dialect: 'mysql'
+})
+
+sequelize.authenticate()
+.then(() => console.log('Database Connected ...'))
+.catch(err => console.log(`Error : ${err}`))
+
+const ProductLog = sequelize.define('PRODUCT_LOGS', {
+	tickets: {
+		type: DataTypes.STRING,
+		allowNull: false
+	},
+	status: {
+		type: DataTypes.STRING
+	},
+	project_id: {
+		type: DataTypes.STRING
+	},
+	priority: {
+		type: DataTypes.STRING
+	},
+	opened_on: {
+		type: DataTypes.DATEONLY
+	},
+	closed_on: {
+		type: DataTypes.DATEONLY
+	}
+})
 
 
-const mysql = require("mysql");
-let connection = mysql.createConnection(sqlConnection.credentialDetails)
-
-connection.connect();
-
-let data = [];
-
-connection.query('SELECT * FROM PRODUCT_LOG', function (error, results, fields) {
-	if (error) throw error;
-	data = results;
-});
-
-app.set("view engine", "ejs");
+let data = []
 
 app.get("/", function(req, res){
+	const productLogs = sequelize.query("SELECT * FROM `product_logs`", { type: QueryTypes.SELECT });
+	console.log("All productLogs:", productLogs);
+
 	res.render("dashboardAdmin", {
 		dashboardAdminData : data,
 		date : date
-	});
-});
+	})
+})
 
-const port = 8080;
+const port = process.env.PORT || 8080
 app.listen(port, function() {
-	console.log('Server started on port :: ' + port);
-	console.log('localhost:' + port);
-});
+	console.log(`Server started on port :: ${port}`)
+})
